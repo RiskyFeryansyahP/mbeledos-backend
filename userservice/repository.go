f673af6363f7bbb8fbf6d20cd73441d6ff5016c9
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2019-12-11 09:35:48 +0700

    repository userservice

---
+++ userservice/repository.go
@@ -0,0 +1,12 @@
+package userservice
+
+import (
+	"context"
+
+	"github.com/confus1on/mbeledos/ent"
+)
+
+type Repository interface {
+	Login(ctx context.Context, phonenumber string) (*ent.User, error)
+	Register(ctx context.Context, user *ent.User) error
+}
