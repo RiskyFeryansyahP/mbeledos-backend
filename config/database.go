425c3a9ddc379f1e4d0d1c1b7e93715d82372a01
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2019-12-11 09:33:55 +0700

    change database mongo to postgreSQL

--- config/database.go
+++ config/database.go
@@ -3,30 +3,26 @@ package config
 import (
 	"context"
 	"log"
-	"time"
 
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"github.com/confus1on/mbeledos/ent"
+	_ "github.com/lib/pq"
 )
 
-var db *mongo.Database
+var db *ent.Client
 
-func New() *mongo.Database {
+func New() *ent.Client {
 	if db != nil {
 		return db
 	}
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://mbeledos:mbeledos123@ds043991.mlab.com:43991/mbeledos").SetRetryWrites(false))
+
+	client, err := ent.Open("postgres", "postgres://ztsuiwtp:PQEjOenZ-xM8IyKEbe70uVO4KQy-QP6e@arjuna.db.elephantsql.com:5432/ztsuiwtp")
 	if err != nil {
-		log.Fatal("Can't Connect to MongoDB", err.Error())
+		log.Fatalf("Failed to connect into postgreSQL %v \n", err.Error())
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatal("Timeout more than 10 sec", err.Error())
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
 	}
 
-	db = client.Database("mbeledos")
-	return db
+	return client
 }
