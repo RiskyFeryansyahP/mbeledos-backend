ec5d5451ffb19b6bd6fe75cfa1d80c81362eca98
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2019-12-11 09:35:25 +0700

    schema fields postgresql ent

---
+++ ent/schema/user.go
@@ -0,0 +1,30 @@
+package schema
+
+import (
+	"github.com/facebookincubator/ent"
+	"github.com/facebookincubator/ent/schema/field"
+)
+
+// User holds the schema definition for the User entity.
+type User struct {
+	ent.Schema
+}
+
+// Fields of the User.
+func (User) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("nohp").
+			Default("Unknown"),
+		field.String("nama").
+			Default("John Doe"),
+		field.String("tgl_lahir").
+			Default("00-00-00"),
+		field.String("alamat").
+			Default("Unknown"),
+	}
+}
+
+// Edges of the User.
+func (User) Edges() []ent.Edge {
+	return nil
+}
