d98533e967dd60cdbc18a31edfbc46aa3fb8351c
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2019-12-11 09:35:34 +0700

    model of user

--- models/user.go
+++ models/user.go
@@ -1,8 +1,8 @@
 package models
 
 type User struct {
-	Name        string `json:"name" bson:"name"`
-	Address     string `json:"address" bson:"address"`
-	Date        string `json:"date" bson:"date"`
-	PhoneNumber string `json:"phonenumber" bson:"phonenumber"`
+	Name        string
+	Address     string
+	Date        string
+	PhoneNumber string
 }
