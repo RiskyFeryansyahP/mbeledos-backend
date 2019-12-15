a12290d42d59e85c10c593b38f2be7c5a638da80
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2019-12-15 11:01:56 +0700

    add repository using postgresql as db

---
+++ bengkelservice/repository/postgre_bengkel.go
@@ -0,0 +1,44 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/confus1on/mbeledos/bengkelservice"
+
+	"github.com/confus1on/mbeledos/ent"
+	"github.com/confus1on/mbeledos/ent/bengkel"
+)
+
+type PostgreSQLBengkelRepository struct {
+	DB *ent.Client
+}
+
+func NewPostgreSQLBengkelRepository(DB *ent.Client) bengkelservice.RepositoryBengkel {
+	return &PostgreSQLBengkelRepository{DB: DB}
+}
+
+func (br *PostgreSQLBengkelRepository) ShowAllBengkel(ctx context.Context) ([]*ent.Bengkel, error) {
+	result, err := br.DB.Bengkel.
+		Query().
+		All(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("Error when get all data bengkel %v", err.Error())
+	}
+
+	return result, nil
+}
+
+func (br *PostgreSQLBengkelRepository) GetSpecificationBengkel(ctx context.Context, kode_bengkel string) (*ent.Bengkel, error) {
+	result, err := br.DB.Bengkel.
+		Query().
+		Where(bengkel.KodeBengkelEQ(kode_bengkel)).
+		Only(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("Error when get specification bengkel : %v", err.Error())
+	}
+
+	return result, nil
+}
