6c98d9ac53c1eabb0fcccca61ae335dc33207813
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-01-22 09:22:53 +0700

    added usecase interface for transactions

--- transactionservice/usecace.go
+++ transactionservice/usecace.go
@@ -1,4 +1,11 @@
 package transactionservice
 
+import (
+	"context"
+
+	"github.com/confus1on/mbeledos/ent"
+)
+
 type UsecaseTransaction interface {
+	InsertTransaction(ctx context.Context, transaction ent.Transaction) error
 }
