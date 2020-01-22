3f3a72aff99f90f4cc60b311cbdb80065758995d
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-01-22 09:22:45 +0700

    added repository interface for transaction

--- transactionservice/repository.go
+++ transactionservice/repository.go
@@ -1,5 +1,11 @@
 package transactionservice
 
+import (
+	"context"
+
+	"github.com/confus1on/mbeledos/ent"
+)
+
 type RepositoryTransaction interface {
-	
+	InsertTransaction(ctx context.Context, transaction ent.Transaction) error
 }
